6393b6db69eeeb9f966fa8619041fd77b04bffcc
Sharvy Ahmed <sharvy2008@gmail.com>
2016-11-09 03:19:11 +0600

    Record will be created and rendered using React.

---
+++ app/assets/javascripts/components/record_form.js
@@ -0,0 +1,82 @@
+RecordForm = React.createClass({
+
+  getInitialState: function() {
+    return {
+      title: '',
+      date: '',
+      amount: ''
+    };
+  },
+
+  render: function() {
+    return React.DOM.form({
+      className: 'form-inline',
+      onSubmit: this.handleSubmit
+    },
+      React.DOM.div({
+        className: 'form-group'
+      },
+        React.DOM.input({
+          type: 'text',
+          className: 'form-control',
+          placeholder: 'Date',
+          name: 'date',
+          value: this.state.date,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.div({
+        className: 'form-group'
+      },
+        React.DOM.input({
+          type: 'text',
+          className: 'form-control',
+          placeholder: 'Title',
+          name: 'title',
+          value: this.state.title,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.div({
+        className: 'form-group'
+      },
+        React.DOM.input({
+          type: 'number',
+          className: 'form-control',
+          placeholder: 'Amount',
+          name: 'amount',
+          value: this.state.amount,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.button({
+        type: 'submit',
+        className: 'btn btn-primary'
+      }, 'Create Record')
+    )
+  },
+
+  handleChange: function(e) {
+    var name = e.target.name;
+    var obj = {};
+    obj[name] = e.target.value;
+
+    return this.setState(obj);
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    return $.post('', {
+      record: this.state
+    }, (function(_this) {
+      return function(data) {
+        _this.props.handleNewRecord(data);
+        return _this.setState(_this.getInitialState);
+      }
+    })(this), 'JSON');
+  },
+
+  valid: function() {
+    return this.state.title && this.state.date && this.state.amount;
+  }
+});
--- app/assets/javascripts/components/records.js
+++ app/assets/javascripts/components/records.js
@@ -1,36 +1,60 @@
 Records = React.createClass({
+
   getInitialState: function() {
     return {
       records: this.props.data
     }
   },
+
   getDefaultProps: function() {
     return {
       records: []
     }
   },
+
+  addRecord: function(record) {
+    records = this.state.records.slice();
+    records.push(record);
+    this.setState({records: records})
+  },
+
   render: function() {
+
     return React.DOM.div({
       className: 'records'
-    }, React.DOM.h2({
+    },
+      React.DOM.h2({
       className: 'title'
     }, 'Records'),
+      React.createElement(RecordForm, {
+      handleNewRecord: this.addRecord
+    }),
+      React.DOM.hr(null),
       React.DOM.table({
         className: 'table table-bordered'
-      }, React.DOM.thead(null, React.DOM.tr(null, React.DOM.th(null, 'Date'), React.DOM.th(null, 'Title'), React.DOM.th(null, 'Amount'))), React.DOM.tbody(null, (function() {
-        var i, len, ref, results;
-        ref = this.state.records;
-        len = ref.length;
-        results = [];
+      },
+        React.DOM.thead(null,
+          React.DOM.tr(null,
+            React.DOM.th(null, 'Date'),
+            React.DOM.th(null, 'Title'),
+            React.DOM.th(null, 'Amount'))),
+        React.DOM.tbody(null, (function() {
+          var i, len, ref, results;
+          ref = this.state.records;
+          len = ref.length;
+          results = [];
 
-        for(i = 0; i < len; i++) {
-          record = ref[i];
-          results.push(React.createElement(Record, {
-            key: record.id,
-            record: record
-          }))
-        }
-        return results;
-      }).call(this))));
+          for(i = 0; i < len; i++) {
+            record = ref[i];
+            results.push(React.createElement(Record, {
+              key: record.id,
+              record: record
+            }))
+          }
+          return results;
+        }).call(this)
+        )
+      )
+    );
   }
 });
