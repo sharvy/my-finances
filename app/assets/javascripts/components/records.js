a94222bd60c7fdf143236bd5471b45f1d90512eb
Sharvy Ahmed <sharvy2008@gmail.com>
2016-11-09 16:38:42 +0600

    Added amount boxes to show total credits, debits and balance.

--- app/assets/javascripts/components/records.js
+++ app/assets/javascripts/components/records.js
@@ -18,6 +18,31 @@ Records = React.createClass({
     this.setState({records: records})
   },
 
+  credits: function() {
+    var credits;
+    credits = this.state.records.filter(function(record) {
+      return record.amount >= 0;
+    });
+
+    return credits.reduce(function(sum, currentElement) {
+      return sum + parseFloat(currentElement.amount);
+    }, 0);
+  },
+
+  debits: function() {
+    debits = this.state.records.filter(function(record) {
+      return record.amount < 0;
+    });
+
+    return debits.reduce(function(sum, currentElement) {
+      return sum + parseFloat(currentElement.amount);
+    }, 0);
+  },
+
+  balance: function() {
+    return this.debits() + this.credits();
+  },
+
   render: function() {
 
     return React.DOM.div({
@@ -26,9 +51,28 @@ Records = React.createClass({
       React.DOM.h2({
       className: 'title'
     }, 'Records'),
+      React.DOM.div({
+        className: 'row'
+      },
+        React.createElement(AmountBox, {
+          type: 'success',
+          amount: this.credits(),
+          text: 'Credit'
+        }),
+        React.createElement(AmountBox, {
+          type: 'danger',
+          amount: this.debits(),
+          text: 'Debit'
+        }),
+        React.createElement(AmountBox, {
+          type: 'info',
+          amount: this.balance(),
+          text: 'Balance'
+        })
+      ),
       React.createElement(RecordForm, {
-      handleNewRecord: this.addRecord
-    }),
+        handleNewRecord: this.addRecord
+      }),
       React.DOM.hr(null),
       React.DOM.table({
         className: 'table table-bordered'
@@ -45,7 +89,7 @@ Records = React.createClass({
           results = [];
 
           for(i = 0; i < len; i++) {
-            record = ref[i];
+            var record = ref[i];
             results.push(React.createElement(Record, {
               key: record.id,
               record: record
